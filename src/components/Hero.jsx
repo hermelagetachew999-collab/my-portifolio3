62d9d5308e9da3e9153bd7455ec7f9537d06599d
hermelagetachew999 <hermelagetachew999@gmail.com>
2025-12-16 14:42:59 +0300

    Update LinkedIn link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -116,7 +116,7 @@ export default function Hero() {
         >
           {/* LinkedIn */}
           <a
-            href="https://www.linkedin.com/in/herru-gech-19490538a"
+            href="https://www.linkedin.com/in/eleny-g-334838391?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             target="_blank"
             rel="noopener noreferrer"
             style={{
